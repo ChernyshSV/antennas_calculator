99995b486b0609960b2069413aa430d3c144f1f7
ChernyshSV <mr.serhii.chernysh@gmail.com>
2025-08-24 14:27:33 +0300

    add DemProvider

---
+++ Fresnel.Core/Dem/FlatDemProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Fresnel.Core.Dem;
+
+public sealed class FlatDemProvider : IDemProvider
+{
+	// Заглушка: рівна земля (0 м).
+	public double GetElevation(double latitudeDeg, double longitudeDeg) => 0.0;
+}
---
+++ Fresnel.Core/Dem/IDemProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Fresnel.Core.Dem;
+
+public interface IDemProvider
+{
+	// Повертає абсолютну висоту над рівнем моря (м).
+	// Реалізацію (SRTM/GDAL) додамо пізніше.
+	double GetElevation(double latitudeDeg, double longitudeDeg);
+}
